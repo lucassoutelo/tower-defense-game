2be748fdcf4c24be0e204c5f4d882475dfa793d3
Lucas Soutelo <56812864+lucassoutelo@users.noreply.github.com>
2022-11-29 10:40:23 -0300

    atirando

--- main.py
+++ main.py
@@ -17,7 +17,12 @@ FPS =  60
 #CARREGAR IMAGENS
 backgrund = pygame.image.load('img/bg4.jpg').convert_alpha()
 torreimg100 = pygame.image.load('img/torre/torre100.png').convert_alpha()
-bala =  pygame.image.load('img/bala.png')
+
+#Imagem de balas
+bala_img =  pygame.image.load('img/bala.png').convert_alpha()
+b_width = bala_img.get_width()
+b_height = bala_img.get_height()
+bala_img = pygame.transform.scale(bala_img, (int(b_width * 0.1), int(b_height * 0.1)))
 
 #definir cores
 WHITE =  (255, 255, 255)
@@ -39,17 +44,55 @@ class Torre():
   def Shoot(self):
     pos = pygame.mouse.get_pos()
     x_dist = pos[0] - self.rect.midleft[0]
-    y_dist = pos[1] - self.rect.midleft[1]
-    self.angle = math.degrees(math.atan2(x_dist, y_dist))
+    y_dist = -(pos[1] - self.rect.midleft[1])
+    self.angle = math.degrees(math.atan2(y_dist, x_dist))
+    self.fired = False
+    
+    #get mouse_click
+    if pygame.mouse.get_pressed()[0] and self.fired == False:
+      self.fired = True
+      bala = Bala(bala_img, self.rect.midleft[0], self.rect.midleft[1], self.angle)
+      grupo_balas.add(bala)
+    #resenatndo mouseclick
+    if pygame.mouse.get_pressed()[0] == False:
+      self.fired = False
 
-    pygame.draw.line(screen, WHITE, (self.rect.midleft[0], self.rect.midleft[1]), (pos))
+    #pygame.draw.line(screen, WHITE, (self.rect.midleft[0], self.rect.midleft[1]), (pos))
   
   def Draw(self):
     self.image = self.image100
     screen.blit(self.image, self.rect)
 
+#CLASSE BALAS
+class Bala(pygame.sprite.Sprite):
+  def __init__(self, image, x, y, angle):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = image
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.angle = math.radians(angle)
+    self.speed = 10
+    self.dx = math.cos(self.angle) * self.speed
+    self.dy = -(math.sin(self.angle) * self.speed)
+
+  def update(self):
+    #se a bala sair da tela
+    if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH or self.rect.bottom < 0 or self.rect.top > SCREEN_HEIGTH:
+      self.kill()
+
+    #mover a bala
+    self.rect.x += self.dx
+    self.rect.y += self.dy
+
+
+
+
 #CRIAR TORRE
 torre = Torre(torreimg100, SCREEN_WIDTH - 250, SCREEN_HEIGTH - 300, 0.2)
+
+#CRIAR GRUPOS
+grupo_balas = pygame.sprite.Group()
 
 #JOGO EM LOOP
 run = True
@@ -61,6 +104,11 @@ while run:
   #desenha torre
   torre.Draw()
   torre.Shoot()
+  #desenha balas
+  grupo_balas.draw(screen)
+
+  #movimenta balas
+  grupo_balas.update()
 
   #eventos
   for event in pygame.event.get():
